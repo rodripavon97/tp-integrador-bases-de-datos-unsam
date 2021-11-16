bf45769b8dcfd8de04ac6c587e793c20ff61bed8
Rodrigo Pavon <rodrigopavongomez@gmail.com>
2021-11-16 15:04:44 -0300

    Agrego la tabla

---
+++ integrador/src/pages/MisDescargas.js
@@ -0,0 +1,11 @@
+import {Box} from '@chakra-ui/layout'
+import Tabla from '../components/tabla';
+
+const MisDescargas = (props) => {
+  return (
+    <Box p={50}>
+      <Tabla />
+    </Box>
+  );
+};
+export default MisDescargas;
