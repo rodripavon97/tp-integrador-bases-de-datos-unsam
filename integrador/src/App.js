8400ae84cb98bc56c687628ec4dfcfa1b4024d15
Guillermo Bianchi <ultraguille2006@gmail.com>
2021-11-19 12:43:48 -0300

    Estilizado de la sidebar, destaca la ruta actual

--- integrador/src/App.js
+++ integrador/src/App.js
@@ -1,17 +1,24 @@
-import './App.css';
-import {ChakraProvider, List} from '@chakra-ui/react'
-import MisDescargas from './pages/MisDescargas';
-import { Box, Flex, HStack, ListItem } from '@chakra-ui/layout';
-import HeaderUsuario from './components/headerUsuario';
+import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import MisDescargas from "./pages/MisDescargas";
+import {
+  Box,
+  Flex,
+  Heading,
+  HStack,
+  ListItem,
+  UnorderedList,
+} from "@chakra-ui/layout";
+import HeaderUsuario from "./components/headerUsuario";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
-  Link,
-} from "react-router-dom"
-import Reporte from './pages/Reporte';
-import Proximamente from './pages/Proximamente';
+  NavLink,
+} from "react-router-dom";
+import Reporte from "./pages/Reporte";
+import Proximamente from "./pages/Proximamente";
 
 function App() {
   return (
@@ -19,31 +26,88 @@ function App() {
       <Box w="100vw" bgColor="gray.400">
         <Flex direction="column" h="100vh" justifyContent="space-between">
           <HeaderUsuario h={3} />
-          <HStack flexGrow="1" h="80%" >
+          <HStack flexGrow="1" h="80%">
             <Router>
-              <Box bg="blue.800" w="20%" h="100%">
-                <List color="white">
-                  <ListItem><Link to="/misEncuestas">* Mis Encuestas</Link></ListItem>
-                  <ListItem><Link to="/reporte">* Reporte de Descargas</Link></ListItem>
-                </List>
+              <Box bg="blue.800" w="20%" h="100%" color="white">
+                <Heading fontSize="xl" pl={2} py={3}>
+                  Perfil
+                </Heading>
+                <UnorderedList pl={5}>
+                  <ListItem>
+                    <NavLink
+                      activeClassName="sidebar-selected-link"
+                      to="/misDatos"
+                    >
+                      Mis Datos
+                    </NavLink>
+                  </ListItem>
+                  <ListItem>
+                    <NavLink
+                      activeClassName="sidebar-selected-link"
+                      to="/seguridad"
+                    >
+                      Seguridad
+                    </NavLink>
+                  </ListItem>
+                </UnorderedList>
+                <Heading fontSize="xl" pl={2} py={3}>
+                  Mis Comentarios
+                </Heading>
+                <UnorderedList pl={5}>
+                  <ListItem>
+                    <NavLink
+                      activeClassName="sidebar-selected-link"
+                      to="/misComentarios"
+                    >
+                      Comentarios
+                    </NavLink>
+                  </ListItem>
+                  <ListItem>
+                    <NavLink
+                      activeClassName="sidebar-selected-link"
+                      to="/misReplicas"
+                    >
+                      Replicas
+                    </NavLink>
+                  </ListItem>
+                </UnorderedList>
+                <Heading fontSize="xl" pl={2} py={3}>
+                  Mis Descargas
+                </Heading>
+                <UnorderedList pl={5}>
+                  <ListItem>
+                    <NavLink
+                      activeClassName="sidebar-selected-link"
+                      to="/misEncuestas"
+                    >
+                      Mis Encuestas
+                    </NavLink>
+                  </ListItem>
+                  <ListItem>
+                    <NavLink
+                      activeClassName="sidebar-selected-link"
+                      to="/reporte"
+                    >
+                      Reporte de Descargas
+                    </NavLink>
+                  </ListItem>
+                </UnorderedList>
               </Box>
               <Box maxH="100%" h="100%" w="full" overflow="auto">
-                  <Switch>
-                    <Route exact path="/">
-                      <Redirect
-                        to="/misEncuestas"
-                      />
-                    </Route>
-                    <Route path="/misEncuestas">
-                      <MisDescargas/>
-                    </Route>
-                    <Route path="/reporte">
-                      <Reporte />
-                    </Route>
-                    <Route path="/*">
-                      <Proximamente />
-                    </Route>
-                  </Switch>
+                <Switch>
+                  <Route exact path="/">
+                    <Redirect to="/misEncuestas" />
+                  </Route>
+                  <Route path="/misEncuestas">
+                    <MisDescargas />
+                  </Route>
+                  <Route path="/reporte">
+                    <Reporte />
+                  </Route>
+                  <Route path="/*">
+                    <Proximamente />
+                  </Route>
+                </Switch>
               </Box>
             </Router>
           </HStack>
